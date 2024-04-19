9fa893deec48df52edd0d761e4ccd466c56d184f
Mukund <137892321+mukund2506@users.noreply.github.com>
2024-04-19 15:27:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function loginWithFacebook() {
     // Additional implementation for Facebook login goes here
 }
 
-// Other JavaScript functions (unchanged)
+// Other JavaScript functions
 function login() {
     var emailOrUsername = document.getElementById("email_or_username").value;
     var password = document.getElementById("password").value;
@@ -31,3 +31,66 @@ function login() {
     console.log("Confirm Password:", confirmPassword);
     window.location.reload();
 }
+//GOOGLE LOGIN
+function loginWithGoogle(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send this directly to your server!
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+  function loginWithGoogle() {
+    gapi.load('auth2', function() {
+      gapi.auth2.init({
+        client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com'
+      }).then(function(auth2) {
+        auth2.signIn().then(function(googleUser) {
+          console.log('Google user signed in:', googleUser);
+          // You can handle the signed-in user here, e.g., send the user data to your server
+        });
+      });
+    });
+  }
+// FACE BOOK LOGIN
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 'your-app-id',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v12.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  };
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+      console.log('Logged into Facebook.');
+      testAPI();
+    } else {
+      console.log('Please log into Facebook.');
+    }
+  }
+
+  function testAPI() {
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+    });
+  }
