5ab4d045c90aed0502383f5a0aa15e861455fd24
Mukund <137892321+mukund2506@users.noreply.github.com>
2024-04-19 16:07:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,3 +94,11 @@ window.fbAsyncInit = function() {
       console.log('Successful login for: ' + response.name);
     });
   }
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
